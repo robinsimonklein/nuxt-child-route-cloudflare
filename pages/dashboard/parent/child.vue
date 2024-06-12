747cd34be91c76d64c7a264d4af291f51d3ea43b
Robin Simonklein <robin.simonklein@mezcalito.fr>
2024-06-12 13:30:34 +0200

    add useFetch

--- pages/dashboard/parent/child.vue
+++ pages/dashboard/parent/child.vue
@@ -1,7 +1,20 @@
 <template>
   <h2>Child route</h2>
+
+  <div>
+    <h4>Posts</h4>
+    <div>
+      <ul>
+        <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import {useFetch} from "nuxt/app";
 
+type Post = { id: number; title: string };
+
+const { data: posts } = await useFetch<Post[]>('https://jsonplaceholder.typicode.com/posts')
 </script>
